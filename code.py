e2d15e340554b555696b819aecc7b4568b1df109
ThePinkPanther77 <74511930+ThePinkPanther77@users.noreply.github.com>
2020-11-18 17:48:16 +0200

    Update code.py

--- code.py
+++ code.py
@@ -5,9 +5,8 @@ import numpy as np
 y=list()
 
 class Graph:
-    '''A class for ploting a function.'''
+    '''A class for ploting a quadratic equation.'''
     def __init__(self,a,b,c,minn,maxx):
-        '''The class constructor.'''
         self.a=a
         self.b=b
         self.c=c
@@ -15,40 +14,28 @@ class Graph:
         self.maxx=maxx
 
     def creator(self):
+        '''A method that creates the graph and adjust the axis. '''
         x=np.linspace(self.minn,self.maxx)
-        #Making an array that contains the values of x 
         for i in x:
             y.append((self.a*i*i)+(self.b*i)+self.c)
-        #Making an array that contains the values of y for each x
         fig = plt.figure()
-        #Creating the plot figure
         ax = SubplotZero(fig, 111)
         fig.add_subplot(ax)
         ax.axis['xzero'].set_visible(True)
         ax.axis['yzero'].set_visible(True)
         for i in ["left", "right", "bottom", "top"]:
                 ax.axis[i].set_visible(False) 
-        #Adjusting the axis
         plt.plot(x,y)
         plt.show()
-        #Showing the plot
 
-
-def main(a,b,c,minn,maxx):
-    '''
-    The function that initializes the Graph class.
-
-    a -- the first parameter
-    b -- the second parameter
-    c -- the third parameter
-    minn -- the minimum value of x
-    maxx -- the maximum value of x
-    '''
+def main():
+    '''A function the initializes the Graph calss.'''
+    print('Enter the parameters:')
+    a,b,c=map(int,input().split())
+    print('Enter the minimum and maximum values of x (respectively):')
+    minn,maxx=map(int,input().split())
     p=Graph(a,b,c,minn,maxx)
     p.creator()
 
-print('Enter the parameters:')
-a,b,c=map(int,input().split())
-print('Enter the minimum and maximum values of x (respectively):')
-minn,maxx=map(int,input().split())
-main(a,b,c,minn,maxx)
+if __name__ == "__main__":
+    main()

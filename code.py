18648e86950fcdeb88b37a264e7ad74ddc2eeff0
ThePinkPanther77 <74511930+ThePinkPanther77@users.noreply.github.com>
2020-11-29 03:54:06 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,8 @@
+from tkinter import *
 from mpl_toolkits.axisartist.axislines import SubplotZero
 import matplotlib.pyplot as plt
 import numpy as np
 
-y=list()
 
 class Graph:
     '''A class for ploting a quadratic equation.'''
@@ -15,6 +15,7 @@ class Graph:
 
     def creator(self):
         '''A method that creates the graph and adjust the axis.'''
+        y=list()
         x=np.linspace(self.minn,self.maxx)
         for i in x:
             y.append((self.a*i*i)+(self.b*i)+self.c)
@@ -24,18 +25,57 @@ class Graph:
         ax.axis['xzero'].set_visible(True)
         ax.axis['yzero'].set_visible(True)
         for i in ["left", "right", "bottom", "top"]:
-                ax.axis[i].set_visible(False) 
+                ax.axis[i].set_visible(False)
         plt.plot(x,y)
         plt.show()
 
-def main():
+def main(event=None):
     '''A function that initializes the calss (Graph).'''
-    print('Enter the parameters:')
-    a,b,c=map(int,input().split())
-    print('Enter the minimum and maximum values of x (respectively):')
-    minn,maxx=map(int,input().split())
-    p=Graph(a,b,c,minn,maxx)
+    a=int(box1.get())
+    b=int(box2.get())
+    c=int(box3.get())
+    minn=int(box4.get())
+    maxx=int(box5.get())
+    p=Graph(a, b, c, minn, maxx)
     p.creator()
 
-if __name__ == "__main__":
-    main()
+
+root =Tk(className="Quadratic Equation Plotter")
+icon = PhotoImage(file = 'Quadratic Equation Plotter.gif')
+root.iconphoto(False, icon) 
+
+
+label1=Label(root,text="Enter the first parameter:")
+box1=Entry(root,width=50)
+
+label2=Label(root,text="Enter the second parameter:")
+box2=Entry(root,width=50)
+
+label3=Label(root,text="Enter the third parameter:")
+box3=Entry(root,width=50)
+
+label4=Label(root,text="Enter the minimum value of x:")
+box4=Entry(root,width=50)
+
+label5=Label(root,text="Enter the maximum value of x:")
+box5=Entry(root,width=50)
+
+button=Button(root,text="Enter",command=main)
+
+root.bind('<Return>',main)
+
+label1.grid(row=1,column=1)
+label2.grid(row=3,column=1)
+label3.grid(row=5,column=1)
+label4.grid(row=7,column=1)
+label5.grid(row=9,column=1)
+
+box1.grid(row=2,column=1)
+box2.grid(row=4,column=1)
+box3.grid(row=6,column=1)
+box4.grid(row=8,column=1)
+box5.grid(row=10,column=1)
+
+button.grid(row=11,column=1)
+
+root.mainloop()
